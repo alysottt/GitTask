cda118f584065ddf3198df767efde5e58016927a
Alexander Vysotsky <alysottt@gmail.com>
2021-04-10 15:39:36 +0600

    Fix index file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Индекс</title>
+    <title>Index</title>
 </head>
 <h2 style="text-align:center">Text</h2>
 <p><?= file_get_contents(__DIR__ . "/file1.txt")?></p>
